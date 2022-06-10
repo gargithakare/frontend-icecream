c42ed31dd0ec1673f8be2499d936f54d22ae81a1
gargithakare <gargithakare06@gmail.com>
2022-06-10 21:28:43 +0530

    First commit

---
+++ menu.js
@@ -0,0 +1,14 @@
+function openMenu(evt, menuName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("menu");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";
+    }
+   //  tablinks = document.getElementsByClassName("tablink");
+   //  for (i = 0; i < x.length; i++) {
+   //     tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
+   //  }
+    document.getElementById(menuName).style.display = "block";
+   //  evt.currentTarget.firstElementChild.className += " w3-red";
+  }
+  document.getElementById("myLink").click();
